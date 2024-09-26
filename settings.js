85bd7c397d304b586a5415b47040d3e9c5998dd2
Livzjasteb <zyxsesi@gmail.com>
2024-09-26 13:41:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ const {
 } = require("./language");
 
 // Website Api (jgn di ganti biar gk eror)
-global.APIs = {
+global.APIs = {"
    alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
 }
 
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "7346494323:AAFyILocwU_0OhXfP9DMNr8L7-e4PBLII9I" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "AlwaysZyy" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ AlwaysZyy×፝֟͜×" //your name
+global.OWNER_NUMBER = "6283826586078" //your telegram number
+global.OWNER = ["https://t.me/AlwaysZyyy", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
